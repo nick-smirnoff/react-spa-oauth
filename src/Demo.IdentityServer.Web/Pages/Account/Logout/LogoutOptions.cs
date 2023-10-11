b4f93126b5a3b5cd9151cf888f8d3b66b94edf4b
Nick Smirnoff <nick.smirnoff@puregym.com>
2023-10-11 14:21:04 +0100

    added duende identity server boilerplate

---
+++ src/Demo.IdentityServer.Web/Pages/Account/Logout/LogoutOptions.cs
@@ -0,0 +1,9 @@
+
+namespace Demo.IdentityServer.Web.Pages.Logout
+{
+    public class LogoutOptions
+    {
+        public static bool ShowLogoutPrompt = true;
+        public static bool AutomaticRedirectAfterSignOut = false;
+    }
+}
